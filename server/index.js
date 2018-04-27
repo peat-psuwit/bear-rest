63331932d431a0a78f97d357b263847d3d39a9f4
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-04-27 21:53:13 +0700

    Step 2: Basic bear rest server
      - A server with capabilities to GET and POST
      - Bonus: random id generation

---
+++ server/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+var router = express.Router();
+
+var bears = [
+    { id: "15642", name: "Bear1" },
+    { id: "54845", name: "Bear2" }
+];
+
+app.use(cors());
+
+router.route('/bears')
+    .get(function (req, res) {
+        res.json(bears);
+    })
+    .post(function(req, res) {
+        var bear = {};
+        bear.name = req.body.name;
+        bear.id = Math.round((Math.random() * 65535)).toString();
+        bears.push(bear);
+        res.json({ message: 'Bear created!' });
+    });
+
+// all of our routes will be prefixed with /api
+app.use('/api', bodyParser.json(), router);
+app.listen(8000);
