a087655a65e84b7861a39c62f16e056887051bf5
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-04-27 22:43:16 +0700

    Step 4: basic redux store
    - package.json: add redux and redux-thunk as dependencies
    - App.js
      - redux store with thunk middleware
      - reducer
      - fetchBearsActionCreator
      - App: subscribe to store & get state

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 const BEAR_API_URL = 'http://localhost:8000/api/bears';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+let reducer = function(state = [], action) {
+  if (action) {
+    if (action.type === 'FETCH_BEAR') {
+      return action.payload;
+    }
+  }
+
+  return state;
+}
+
+let store = createStoreWithMiddleware(reducer);
+
+let fetchBearsActionCreator = function() {
+  return (dispatch) => {
+    return axios.get(BEAR_API_URL)
+      .then( (response) => {
+        console.log('fetchBears payload: ', response.data);
+
+        dispatch({ type: 'FETCH_BEAR', payload: response.data });
+      });
+  };
+};
 
 class App extends Component {
   constructor(props) {
@@ -13,14 +40,13 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get(BEAR_API_URL)
-      .then( (response) => {
-        console.log('Bears:', response.data);
-
-        this.setState({
-          bears: response.data
-        });
+    store.subscribe( () => {
+      this.setState({
+        bears: store.getState()
       });
+    });
+
+    store.dispatch(fetchBearsActionCreator());
   }
 
   renderSingleBear(bear) {
