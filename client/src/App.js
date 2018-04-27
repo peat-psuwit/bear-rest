37cc28b8f19719fc3711968baa24634e13fc0e76
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-04-27 22:12:40 +0700

    Step 3: list bears
      - package;json: add axios as a dependency
      - App.js: fetch bears from server and put in state
      - App.js: render bears

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const BEAR_API_URL = 'http://localhost:8000/api/bears';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bears: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(BEAR_API_URL)
+      .then( (response) => {
+        console.log('Bears:', response.data);
+
+        this.setState({
+          bears: response.data
+        });
+      });
+  }
+
+  renderSingleBear(bear) {
     return (
-      <div>Hello, world!</div>
+      <div key={bear.id}>{bear.name}</div>
+    );
+  }
+
+  render() {
+    let bears = this.state.bears;
+
+    return (
+      <div>
+        { bears.map( this.renderSingleBear ) }
+      </div>
     );
   }
 }
